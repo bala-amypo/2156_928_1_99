c61f98387aae82befc65811541cc0a60140247f8
bala-amypo <bala@amypo.in>
2025-12-18 09:15:03 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/DepreciationRuleRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.DepreciationRule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepreciationRuleRepository
+        extends JpaRepository<DepreciationRule, Long> {
+}
