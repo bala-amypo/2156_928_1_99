2cd5106fac145dfc48ea84b7ec6b91089dd5716d
bala-amypo <bala@amypo.in>
2025-12-25 14:25:20 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,17 @@
-package com.example.demo;
+spring.application.name=sql
+# Don't change the port
+server.port = 9001
+# for https
+server.forward-headers-strategy=framework 
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DemoApplication {
+spring.datasource.url=jdbc:mysql://localhost:3306/transport_pro?createDatabaseIfNotExist=true
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+spring.datasource.username=root
+spring.datasource.password=Amypo
+spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver
 
-}
+spring.jpa.hibernate.ddl-auto=update
+spring.jpa.show-sql=true
+spring.jpa.properties.hibernate.format_sql=true
+spring.jpa.database-platform=org.hibernate.dialect.MySQL8Dialect
